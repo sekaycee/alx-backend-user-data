91ce2f5b9256c4cb6f4c20cb110de3142660c6b9
Kay Cee <se.kaycee@gmail.com>
2023-08-02 23:23:06 +0100

    feat: check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
-""" password encrypting module. """
+''' password encrypting module. '''
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """ hash a password using a random salt. """
+    ''' hash a password using a random salt. '''
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' check if a password hash was formed from the given password. '''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
