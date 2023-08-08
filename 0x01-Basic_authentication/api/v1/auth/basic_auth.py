e7356af25dfdd9817c49139248efaedf58dcdc54
Kay Cee <sekaycee@gmail.com>
2023-08-08 19:46:40 +0100

    feat: add Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python3
-"""Basic authentication module for the API.
-"""
+'''Basic authentication module for the API.
+'''
 import re
 import base64
 import binascii
 from typing import Tuple, TypeVar
-
 from .auth import Auth
 from models.user import User
 
 
 class BasicAuth(Auth):
-    """Basic authentication class."""
+    '''Basic authentication class.'''
 
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
-        """Extracts the Base64 part of the Authorization header
+        '''Extract the Base64 part of the Authorization header
         for a Basic Authentication.
-        """
+        '''
         if type(authorization_header) == str:
             pattern = r'Basic (?P<token>.+)'
             field_match = re.fullmatch(pattern, authorization_header.strip())
             if field_match is not None:
                 return field_match.group('token')
         return None
+
+
+    def decode_base64_authorization_header(
+            self,
+            base64_authorization_header: str,
+            ) -> str:
+        '''Decode a base64-encoded authorization header.'''
+        if type(base64_authorization_header) == str:
+            try:
+                res = base64.b64decode(
+                    base64_authorization_header,
+                    validate=True,
+                )
+                return res.decode('utf-8')
+            except (binascii.Error, UnicodeDecodeError):
+                return None
