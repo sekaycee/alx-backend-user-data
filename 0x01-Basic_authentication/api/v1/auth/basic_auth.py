584437ef65606527244c06ce4b96681e420f04c8
Kay Cee <sekaycee@gmail.com>
2023-08-08 19:40:29 +0100

    feat: add Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Basic authentication module for the API.
+"""
+import re
+import base64
+import binascii
+from typing import Tuple, TypeVar
+
+from .auth import Auth
+from models.user import User
+
+
+class BasicAuth(Auth):
