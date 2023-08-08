668fe8daa56ea54108b2e61575a6e7971fb1ea2d
Kay Cee <sekaycee@gmail.com>
2023-08-08 19:42:30 +0100

    feat: add  Basic - Base64 part

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -11,3 +11,17 @@ from models.user import User
 
 
 class BasicAuth(Auth):
+    """Basic authentication class."""
+
+    def extract_base64_authorization_header(
+            self,
+            authorization_header: str) -> str:
+        """Extracts the Base64 part of the Authorization header
+        for a Basic Authentication.
+        """
+        if type(authorization_header) == str:
+            pattern = r'Basic (?P<token>.+)'
+            field_match = re.fullmatch(pattern, authorization_header.strip())
+            if field_match is not None:
+                return field_match.group('token')
+        return None
