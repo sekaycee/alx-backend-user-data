f8b2118aed23ca4dcc0f062d6ed55539cc22811a
Kay Cee <se.kaycee@gmail.com>
2023-08-10 20:35:28 +0100

    feat: create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -7,4 +7,12 @@ from models.user import User
 
 class SessionAuth(Auth):
     """ Session Auth class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create a Session ID for a user_id """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
