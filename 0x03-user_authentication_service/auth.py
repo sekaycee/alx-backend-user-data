d8215a5afcb593f4c833792b1574ff388b585221
Kay Cee <se.kaycee@gmail.com>
2023-08-15 02:38:51 +0100

    feat: find user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -57,3 +57,12 @@ class Auth:
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
             return session_id
+
+    def get_user_from_session_id(self, session_id: str) -> str:
+        """ return a string or None """
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+        else:
+            return user
