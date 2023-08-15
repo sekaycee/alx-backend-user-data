6330d1b77cf3577ccbce76428e26e215c88f676d
Kay Cee <se.kaycee@gmail.com>
2023-08-15 02:45:23 +0100

    feat: generate a reset password token

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -73,3 +73,14 @@ class Auth:
             self._db.update_user(user_id, session_id=None)
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ if it exists, generate a UUID and update the user’s reset_token
+            database field. Return the token """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            raise ValueError
+        token = _generate_uuid()
+        self._db.update_user(user.id, reset_token=token)
+        return token
